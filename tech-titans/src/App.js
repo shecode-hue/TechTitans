a22e79a651d1192b4794cb9f6d00bfb530bec6e6
shecode-hue <shainkampasi@gmail.com>
2023-10-07 14:03:18 +0200

    new nav

--- tech-titans/src/App.js
+++ tech-titans/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./pages/Header/header";
+import Footer from "./pages/Footer/footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ tech-titans/src/pages/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css"; 
+
+function Footer() {
+  return (
+    <footer>
+      <div className="glowing-text">TechTitans</div>
+    </footer>
+  );
+}
+
+export default Footer;
